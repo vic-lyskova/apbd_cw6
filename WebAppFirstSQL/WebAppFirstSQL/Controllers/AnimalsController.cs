1beedc3f26f26afd265327c952b6ee068c2b4d61
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-16 11:17:17 +0200

    Included SqlClient and opened SQL connection for testing purposes

--- WebAppFirstSQL/WebAppFirstSQL/Controllers/AnimalsController.cs
+++ WebAppFirstSQL/WebAppFirstSQL/Controllers/AnimalsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace WebAppFirstSQL.Controllers;
 
@@ -7,9 +8,18 @@ namespace WebAppFirstSQL.Controllers;
 [Route("api/[controller]")]
 public class AnimalsController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
     [HttpGet]
     public IActionResult GetAnimals()
     {
+        
+        SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
         return Ok();
     }
 }
