dc95f1daccd456ecdb2543fa5c04ee25c1dda06e
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-16 10:57:20 +0200

    Added controller AnimalsController and used controllers in program

---
+++ WebAppFirstSQL/WebAppFirstSQL/Controllers/AnimalsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppFirstSQL.Controllers;
+
+[ApiController]
+// [Route("api/animals")]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    
+}
