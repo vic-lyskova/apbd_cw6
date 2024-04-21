c9a036a69775d535451656fff967d673efadbacb
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-21 22:36:21 +0200

    Completed HttpGet request handler with required logic

--- WebAppFirstSQL/WebAppFirstSQL/Controllers/AnimalsController.cs
+++ WebAppFirstSQL/WebAppFirstSQL/Controllers/AnimalsController.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using WebAppFirstSQL.DTOs;
@@ -11,14 +12,25 @@ namespace WebAppFirstSQL.Controllers;
 public class AnimalsController : ControllerBase
 {
     private readonly IConfiguration _configuration;
+
     public AnimalsController(IConfiguration configuration)
     {
         _configuration = configuration;
     }
-    
+
     [HttpGet]
-    public IActionResult GetAnimals()
+    public IActionResult GetAnimals(string orderBy = "name")
     {
+        switch (orderBy.ToLower())
+        {
+            case "name": break;
+            case "description": break;
+            case "category": break;
+            case "area": break;
+            default:
+                return BadRequest();
+        }
+
         //Open connection
         using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
         connection.Open();
@@ -26,25 +38,33 @@ public class AnimalsController : ControllerBase
         //Define command
         SqlCommand command = new SqlCommand();
         command.Connection = connection;
-        command.CommandText = "SELECT * FROM ANIMAL;";
+        command.CommandText = "SELECT * FROM ANIMAL ORDER BY " + orderBy;
 
         //Execute command
         var reader = command.ExecuteReader();
 
-        List<Animal> animals = new List<Animal>();
+        var animals = new List<Animal>();
 
-        int idAnimalOriginal = reader.GetOrdinal("IdAnimal");
-        int nameOrdinal = reader.GetOrdinal("Name");
-        
+        var idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        var nameOrdinal = reader.GetOrdinal("Name");
+        var descriptionOrdinal = reader.GetOrdinal("Description");
+        var categoryOrdinal = reader.GetOrdinal("Category");
+        var areaOrdinal = reader.GetOrdinal("Area");
+
         while (reader.Read())
         {
+            string? description = reader.IsDBNull(descriptionOrdinal) ? null : reader.GetString(descriptionOrdinal);
+
             animals.Add(new Animal()
             {
-                IdAnimal = reader.GetInt32(idAnimalOriginal),
-                Name = reader.GetString(nameOrdinal)
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal),
+                Description = description,
+                Category = reader.GetString(categoryOrdinal),
+                Area = reader.GetString(areaOrdinal)
             });
         }
-            
+
         return Ok(animals);
     }
 
@@ -58,12 +78,12 @@ public class AnimalsController : ControllerBase
         //Define command
         using SqlCommand command = new SqlCommand();
         command.Connection = connection;
-        
+
         command.CommandText = "INSERT INTO ANIMAL VALUES (@animalName, @, @...);";
         command.Parameters.AddWithValue("@animalName", animal.Name);
 
         command.ExecuteNonQuery();
-        
+
         return Created("", null);
     }
 }
