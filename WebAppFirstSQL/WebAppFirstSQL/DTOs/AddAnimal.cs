a6c28c8c46dbbe20375bb0b609f7e93399ea7084
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-21 23:11:11 +0200

    Completed DTO AddAnimal with with necessary properties

--- WebAppFirstSQL/WebAppFirstSQL/DTOs/AddAnimal.cs
+++ WebAppFirstSQL/WebAppFirstSQL/DTOs/AddAnimal.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace WebAppFirstSQL.DTOs;
 
@@ -8,6 +10,17 @@ public class AddAnimal
     [MinLength(1)]
     [MaxLength(200)]
     public string Name { get; set; }
+    
     [MaxLength(200)]
     public string? Description { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    [MaxLength(200)]
+    public string Category { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    [MaxLength(200)]
+    public string Area { get; set; }
 }
