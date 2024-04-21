d5683db2ca384838db02c0b9093690804d73ad68
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-21 22:31:18 +0200

    Added all required properties

--- WebAppFirstSQL/WebAppFirstSQL/Models/Animal.cs
+++ WebAppFirstSQL/WebAppFirstSQL/Models/Animal.cs
@@ -4,4 +4,7 @@ public class Animal
 {
     public int IdAnimal { get; set; }
     public string Name { get; set; }
+    public string? Description { get; set; }
+    public string Category { get; set; }
+    public string Area { get; set; }
 }
